06d05cd2696ead8cbf142d460bae9cf5f28eb65c
erezmb <erezmb@gmail.com>
2024-04-19 06:17:35 -0700

    finished steing shrinkage

--- party.stance.R
+++ party.stance.R
@@ -401,7 +401,8 @@ lda <- function(xt, yt, xv) {
   yv.hat <- unname(cl[apply(yv.hat, 1, which.max)])
   yv.hat
 }
-lda.bad.stein <- function(xt, yt, xv) {
+lda.stein.a <- function(xt, yt, xv) {
+  ### linear discriminant analysis with stein shrinkage N(0,A) ###
   cl <- sort(unique(yt))
   n <- as.numeric(table(yt))
   pi <- prop.table(n)
@@ -416,6 +417,30 @@ lda.bad.stein <- function(xt, yt, xv) {
   a <- mean(t(t(z^2) - 1/n))
   theta <- t(t(z) * (1 - 1/(1 + a * n)))
   mu.adj <- solve(r) %*% theta
+  b <- inv.sigma %*% mu.adj # y.hat = argmax(b*x + a)
+  a <- as.numeric(log(pi) - diag(t(mu.adj) %*% b / 2))
+  yv.hat <- t(t(xv %*% b) + a)
+  yv.hat <- unname(cl[apply(yv.hat, 1, which.max)])
+  yv.hat
+}
+lda.stein.ma <- function(xt, yt, xv) {
+  ### linear discriminant analysis with stein shrinkage N(m,A) ###
+  cl <- sort(unique(yt))
+  n <- as.numeric(table(yt))
+  pi <- prop.table(n)
+  mu <- sapply(cl, function(i) colMeans(xt[yt == i,,drop=F]))
+  sigma <- Reduce("+", lapply(cl, function(i) {
+    xtc <- t(t(xt[yt == i,,drop=F]) - mu[,match(i, cl)]) # xt centered
+    t(xtc) %*% xtc
+  }), 0) / (nrow(xt) - length(cl))
+  inv.sigma <- solve(sigma)
+  r <- chol(inv.sigma)
+  inv.r <- solve(r)
+  z <- r %*% mu
+  m <- rowMeans(z)
+  a <- sum((z - m)^2) / nrow(z) / (ncol(z) - 1) - mean(1/n)
+  u <- as.numeric(inv.r %*% m)
+  mu.adj <- u + t(t(mu - u) * (1 - 1/(1 + a * n)))
   b <- inv.sigma %*% mu.adj # y.hat = argmax(b*x + a)
   a <- as.numeric(log(pi) - diag(t(mu.adj) %*% b / 2))
   yv.hat <- t(t(xv %*% b) + a)
@@ -497,8 +522,8 @@ predict.party.alignment <- function(data, train.frac, nfolds, fdr.q) {
     # predictions
     yv.hat.baseline <- rep(which.max(table(yt)), nrow(xv)) # predict mode
     yv.hat.lda <- lda(xt, yt, xv) # LDA
-    yv.hat.qda <- qda(xt, yt, xv, 0.99) # QDA, no shrinkage
-    yv.hat.lda.bad.stein <- lda.bad.stein(xt, yt, xv)
+    yv.hat.lda.stein.a <- lda.stein.a(xt, yt, xv)
+    yv.hat.lda.stein.ma <- lda.stein.ma(xt, yt, xv)
     # QDA with shrinkage determined by cross validation
     fac <- floor((1:nrow(xt) - 1) / nrow(xt) * nfolds) + 1
     shrinkage.vals <- 0:19/20
@@ -515,14 +540,11 @@ predict.party.alignment <- function(data, train.frac, nfolds, fdr.q) {
       })
     shrinkage <- shrinkage.vals[which.max(rowSums(cv.res))]
     yv.hat.qda.cv <- qda(xt, yt, xv, shrinkage)
-    # random forest
-    #yv.hat.rf <- predict(randomForest(x=xt, y=as.factor(yt)), xv)
     # score predictions
     yv <- as.numeric(dv$player1.party)
     score.predictions(yv, baseline=yv.hat.baseline, lda=yv.hat.lda,
-                      #qda=yv.hat.qda,
-                      #rf=yv.hat.rf,
-                      lda.bad.stein=yv.hat.lda.bad.stein,
+                      lda.stein.a=yv.hat.lda.stein.a,
+                      lda.stein.ma=yv.hat.lda.stein.ma,
                       qda.cv=yv.hat.qda.cv)
   })
   names(out) <- countries
